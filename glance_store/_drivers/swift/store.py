54f673d4133c1244e2555e7ceb3344c104a74154
Flavio Percoco <flaper87@gmail.com>
2014-09-05 14:28:58 +0200

    Align swift's store API
    swift's store get method was lacking of 2 keyword arguments. This patch
    adds them to the API.
    
    Change-Id: I8f67380b4ca07261bc3d4e6fbd201189d1087a96

--- glance_store/_drivers/swift/store.py
+++ glance_store/_drivers/swift/store.py
@@ -403,7 +403,7 @@ class BaseStore(driver.Store):
             reason = (_("Location credentials are invalid"))
             raise exceptions.BadStoreUri(message=reason)
 
-    def get(self, location, connection=None, context=None):
+    def get(self, location, connection=None, offset=0, chunk_size=None, context=None):
         location = location.store_location
         (resp_headers, resp_body) = self._get_object(location, connection,
                                                      context=context)
