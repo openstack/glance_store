615641698e61e4759cdfe51031e18a892ccec3a8
Masashi Ozawa <mozawa@cloudian.com>
2014-09-11 07:40:27 +0000

    Backporting S3 multi-part upload functionality to glace_store
    This commits is to backport S3 multi-part upload functionality
    from the old glance store package to glace_store library.
    
    In addition, refactored the write chunk processing to S3 for
    a large object and dynamic upload part size calculation to
    be able to store large objects via S3 multi-part upload.
    
    Change-Id: Ie01bbf33db5a5ba519c5701ef9475fc0437cd6de

--- glance_store/_drivers/s3.py
+++ glance_store/_drivers/s3.py
@@ -18,11 +18,15 @@
 import hashlib
 import httplib
 import logging
+import math
 import re
 import tempfile
 import urlparse
 
+import boto.exception
+import eventlet
 from oslo.config import cfg
+import six
 
 import glance_store
 from glance_store.common import utils
@@ -33,7 +37,14 @@ import glance_store.location
 from glance_store.openstack.common import units
 
 LOG = logging.getLogger(__name__)
+_LE = glance_store.i18n._LE
+_LI = glance_store.i18n._LI
 
+DEFAULT_LARGE_OBJECT_SIZE = 100          # 100M
+DEFAULT_LARGE_OBJECT_CHUNK_SIZE = 10     # 10M
+DEFAULT_LARGE_OBJECT_MIN_CHUNK_SIZE = 5  # 5M
+DEFAULT_THREAD_POOLS = 10                # 10 pools
+MAX_PART_NUM = 10000                     # 10000 upload parts
 
 _S3_OPTS = [
     cfg.StrOpt('s3_store_host',
@@ -54,7 +65,77 @@ _S3_OPTS = [
     cfg.StrOpt('s3_store_bucket_url_format', default='subdomain',
                help=_('The S3 calling format used to determine the bucket. '
                       'Either subdomain or path can be used.')),
+    cfg.IntOpt('s3_store_large_object_size',
+               default=DEFAULT_LARGE_OBJECT_SIZE,
+               help=_('What size, in MB, should S3 start chunking image files '
+                      'and do a multipart upload in S3.')),
+    cfg.IntOpt('s3_store_large_object_chunk_size',
+               default=DEFAULT_LARGE_OBJECT_CHUNK_SIZE,
+               help=_('What multipart upload part size, in MB, should S3 use '
+                      'when uploading parts. The size must be greater than or '
+                      'equal to 5M.')),
+    cfg.IntOpt('s3_store_thread_pools', default=DEFAULT_THREAD_POOLS,
+               help=_('The number of thread pools to perform a multipart '
+                      'upload in S3.')),
 ]
+
+
+class UploadPart:
+
+    """
+    The class for the upload part
+    """
+
+    def __init__(self, mpu, fp, partnum, chunks):
+        self.mpu = mpu
+        self.partnum = partnum
+        self.fp = fp
+        self.size = 0
+        self.chunks = chunks
+        self.etag = {}  # partnum -> etag
+        self.success = True
+
+
+def run_upload(part):
+    """
+    Upload the upload part into S3 and set returned etag and size
+    to its part info.
+    """
+    pnum = part.partnum
+    bsize = part.chunks
+    LOG.info(_LI("Uploading upload part in S3 partnum=%(pnum)d, "
+                 "size=%(bsize)d, key=%(key)s, UploadId=%(UploadId)s") %
+             {'pnum': pnum,
+              'bsize': bsize,
+              'key': part.mpu.key_name,
+              'UploadId': part.mpu.id})
+
+    try:
+        key = part.mpu.upload_part_from_file(part.fp,
+                                             part_num=part.partnum,
+                                             size=bsize)
+        part.etag[part.partnum] = key.etag
+        part.size = key.size
+    except boto.exception.BotoServerError as e:
+        status = e.status
+        reason = e.reason
+        LOG.error(_LE("Failed to upload part in S3 partnum=%(pnum)d, "
+                      "size=%(bsize)d, status=%(status)d, "
+                      "reason=%(reason)s") %
+                  {'pnum': pnum,
+                   'bsize': bsize,
+                   'status': status,
+                   'reason': reason})
+        part.success = False
+    except Exception as e:
+        LOG.error(_LE("Failed to upload part in S3 partnum=%(pnum)d, "
+                      "size=%(bsize)d due to internal error: %(err)s") %
+                  {'pnum': pnum,
+                   'bsize': bsize,
+                   'err': e})
+        part.success = False
+    finally:
+        part.fp.close()
 
 
 class StoreLocation(glance_store.location.StoreLocation):
@@ -120,7 +201,7 @@ class StoreLocation(glance_store.location.StoreLocation):
                        "s3+https:// scheme, like so: "
                        "s3+https://accesskey:secretkey@"
                        "s3.amazonaws.com/bucket/key-id")
-            LOG.info(_("Invalid store uri: %s") % reason)
+            LOG.info(_LI("Invalid store uri: %s") % reason)
             raise exceptions.BadStoreUri(message=reason)
 
         pieces = urlparse.urlparse(uri)
@@ -146,7 +227,7 @@ class StoreLocation(glance_store.location.StoreLocation):
                 self.accesskey = access_key
                 self.secretkey = secret_key
             except IndexError:
-                reason = _("Badly formed S3 credentials %s") % creds
+                reason = _("Badly formed S3 credentials")
                 LOG.info(reason)
                 raise exceptions.BadStoreUri(message=reason)
         else:
@@ -162,7 +243,7 @@ class StoreLocation(glance_store.location.StoreLocation):
                 reason = _("Badly formed S3 URI. Missing s3 service URL.")
                 raise exceptions.BadStoreUri(message=reason)
         except IndexError:
-            reason = _("Badly formed S3 URI: %s") % uri
+            reason = _("Badly formed S3 URI")
             LOG.info(reason)
             raise exceptions.BadStoreUri(message=reason)
 
@@ -250,11 +331,33 @@ class Store(glance_store.driver.Store):
         buffer_dir = self.conf.glance_store.s3_store_object_buffer_dir
         self.s3_store_object_buffer_dir = buffer_dir
 
+        _s3_obj_size = self._option_get('s3_store_large_object_size')
+        self.s3_store_large_object_size = _s3_obj_size * units.Mi
+        _s3_ck_size = self._option_get('s3_store_large_object_chunk_size')
+        _s3_ck_min = DEFAULT_LARGE_OBJECT_MIN_CHUNK_SIZE
+        if _s3_ck_size < _s3_ck_min:
+            reason = (_("s3_store_large_object_chunk_size must be at "
+                        "least %(_s3_ck_min)d MB. "
+                        "You configured it as %(_s3_ck_size)d MB") %
+                      {'_s3_ck_min': _s3_ck_min,
+                       '_s3_ck_size': _s3_ck_size})
+            LOG.error(reason)
+            raise exceptions.BadStoreConfiguration(store_name="s3",
+                                                   reason=reason)
+        self.s3_store_large_object_chunk_size = _s3_ck_size * units.Mi
+        self.s3_store_thread_pools = self._option_get('s3_store_thread_pools')
+        if self.s3_store_thread_pools <= 0:
+            reason = (_("s3_store_thread_pools must be a positive "
+                        "integer. %s") % self.s3_store_thread_pools)
+            LOG.error(reason)
+            raise exceptions.BadStoreConfiguration(store_name="s3",
+                                                   reason=reason)
+
     def _option_get(self, param):
         result = getattr(self.conf.glance_store, param)
         if not result:
-            reason = (_("Could not find %(param)s in configuration "
-                        "options.") % {'param': param})
+            reason = ("Could not find %(param)s in configuration "
+                      "options." % {'param': param})
             LOG.debug(reason)
             raise exceptions.BadStoreConfiguration(store_name="s3",
                                                    reason=reason)
@@ -311,12 +414,12 @@ class Store(glance_store.driver.Store):
 
         key = get_key(bucket_obj, loc.key)
 
-        msg = _("Retrieved image object from S3 using (s3_host=%(s3_host)s, "
-                "access_key=%(accesskey)s, bucket=%(bucket)s, "
-                "key=%(obj_name)s)") % ({'s3_host': loc.s3serviceurl,
-                                         'accesskey': loc.accesskey,
-                                         'bucket': loc.bucket,
-                                         'obj_name': loc.key})
+        msg = ("Retrieved image object from S3 using (s3_host=%(s3_host)s, "
+               "access_key=%(accesskey)s, bucket=%(bucket)s, "
+               "key=%(obj_name)s)" % ({'s3_host': loc.s3serviceurl,
+                                       'accesskey': loc.accesskey,
+                                       'bucket': loc.bucket,
+                                       'obj_name': loc.key}))
         LOG.debug(msg)
 
         return key
@@ -366,17 +469,11 @@ class Store(glance_store.driver.Store):
 
         bucket_obj = get_bucket(s3_conn, self.bucket)
         obj_name = str(image_id)
-
-        def _sanitize(uri):
-            return re.sub('//.*:.*@',
-                          '//s3_store_secret_key:s3_store_access_key@',
-                          uri)
-
         key = bucket_obj.get_key(obj_name)
         if key and key.exists():
             raise exceptions.Duplicate(message=_("S3 already has an image at "
                                                  "location %s") %
-                                       _sanitize(loc.get_uri()))
+                                       self._sanitize(loc.get_uri()))
 
         msg = _("Adding image object to S3 using (s3_host=%(s3_host)s, "
                 "access_key=%(access_key)s, bucket=%(bucket)s, "
@@ -385,6 +482,29 @@ class Store(glance_store.driver.Store):
                                          'bucket': self.bucket,
                                          'obj_name': obj_name})
         LOG.debug(msg)
+        LOG.debug("Uploading an image file to S3 for %s" %
+                  self._sanitize(loc.get_uri()))
+
+        if image_size < self.s3_store_large_object_size:
+            return self.add_singlepart(image_file, bucket_obj, obj_name, loc)
+        else:
+            return self.add_multipart(image_file, image_size, bucket_obj,
+                                      obj_name, loc)
+
+    def _sanitize(self, uri):
+        return re.sub('//.*:.*@',
+                      '//s3_store_secret_key:s3_store_access_key@',
+                      uri)
+
+    def add_singlepart(self, image_file, bucket_obj, obj_name, loc):
+        """
+        Stores an image file with a single part upload to S3 backend
+
+        :param image_file: The image data to write, as a file-like object
+        :param bucket_obj: S3 bucket object
+        :param obj_name: The object name to be stored(image identifier)
+        :loc: The Store Location Info
+        """
 
         key = bucket_obj.new_key(obj_name)
 
@@ -400,8 +520,8 @@ class Store(glance_store.driver.Store):
         # take this opportunity to calculate the image checksum while
         # writing the tempfile, so we don't need to call key.compute_md5()
 
-        msg = _("Writing request body file to temporary file "
-                "for %s") % _sanitize(loc.get_uri())
+        msg = ("Writing request body file to temporary file "
+               "for %s") % self._sanitize(loc.get_uri())
         LOG.debug(msg)
 
         tmpdir = self.s3_store_object_buffer_dir
@@ -412,21 +532,121 @@ class Store(glance_store.driver.Store):
             temp_file.write(chunk)
         temp_file.flush()
 
-        msg = (_("Uploading temporary file to S3 for %s") %
-               _sanitize(loc.get_uri()))
+        msg = ("Uploading temporary file to S3 "
+               "for %s") % self._sanitize(loc.get_uri())
         LOG.debug(msg)
 
         # OK, now upload the data into the key
-        key.set_contents_from_file(open(temp_file.name, 'r+b'), replace=False)
+        key.set_contents_from_file(open(temp_file.name, 'rb'),
+                                   replace=False)
         size = key.size
         checksum_hex = checksum.hexdigest()
 
-        LOG.debug(_("Wrote %(size)d bytes to S3 key named %(obj_name)s "
-                    "with checksum %(checksum_hex)s"),
-                  {'size': size, 'obj_name': obj_name,
+        LOG.debug("Wrote %(size)d bytes to S3 key named %(obj_name)s "
+                  "with checksum %(checksum_hex)s" %
+                  {'size': size,
+                   'obj_name': obj_name,
                    'checksum_hex': checksum_hex})
 
         return (loc.get_uri(), size, checksum_hex, {})
+
+    def add_multipart(self, image_file, image_size, bucket_obj, obj_name, loc):
+        """
+        Stores an image file with a multi part upload to S3 backend
+
+        :param image_file: The image data to write, as a file-like object
+        :param bucket_obj: S3 bucket object
+        :param obj_name: The object name to be stored(image identifier)
+        :loc: The Store Location Info
+        """
+
+        checksum = hashlib.md5()
+        pool_size = self.s3_store_thread_pools
+        pool = eventlet.greenpool.GreenPool(size=pool_size)
+        mpu = bucket_obj.initiate_multipart_upload(obj_name)
+        LOG.debug("Multipart initiate key=%(obj_name)s, "
+                  "UploadId=%(UploadId)s" %
+                  {'obj_name': obj_name,
+                   'UploadId': mpu.id})
+        cstart = 0
+        plist = []
+
+        chunk_size = int(math.ceil(float(image_size) / MAX_PART_NUM))
+        write_chunk_size = max(self.s3_store_large_object_chunk_size,
+                               chunk_size)
+        it = utils.chunkreadable(image_file, self.WRITE_CHUNKSIZE)
+        buffered_chunk = ''
+        while True:
+            try:
+                buffered_clen = len(buffered_chunk)
+                if buffered_clen < write_chunk_size:
+                    # keep reading data
+                    read_chunk = next(it)
+                    buffered_chunk += read_chunk
+                    continue
+                else:
+                    write_chunk = buffered_chunk[:write_chunk_size]
+                    remained_data = buffered_chunk[write_chunk_size:]
+                    checksum.update(write_chunk)
+                    fp = six.BytesIO(write_chunk)
+                    fp.seek(0)
+                    part = UploadPart(mpu, fp, cstart + 1, len(write_chunk))
+                    pool.spawn_n(run_upload, part)
+                    plist.append(part)
+                    cstart += 1
+                    buffered_chunk = remained_data
+            except StopIteration:
+                if len(buffered_chunk) > 0:
+                    # Write the last chunk data
+                    write_chunk = buffered_chunk
+                    checksum.update(write_chunk)
+                    fp = six.BytesIO(write_chunk)
+                    fp.seek(0)
+                    part = UploadPart(mpu, fp, cstart + 1, len(write_chunk))
+                    pool.spawn_n(run_upload, part)
+                    plist.append(part)
+                break
+
+        pedict = {}
+        total_size = 0
+        pool.waitall()
+
+        for part in plist:
+            pedict.update(part.etag)
+            total_size += part.size
+
+        success = True
+        for part in plist:
+            if not part.success:
+                success = False
+
+        if success:
+            # Complete
+            xml = get_mpu_xml(pedict)
+            bucket_obj.complete_multipart_upload(obj_name,
+                                                 mpu.id,
+                                                 xml)
+            checksum_hex = checksum.hexdigest()
+            LOG.info(_LI("Multipart complete key=%(obj_name)s "
+                         "UploadId=%(UploadId)s "
+                         "Wrote %(total_size)d bytes to S3 key"
+                         "named %(obj_name)s "
+                         "with checksum %(checksum_hex)s") %
+                     {'obj_name': obj_name,
+                      'UploadId': mpu.id,
+                      'total_size': total_size,
+                      'obj_name': obj_name,
+                      'checksum_hex': checksum_hex})
+            return (loc.get_uri(), total_size, checksum_hex, {})
+        else:
+            # Abort
+            bucket_obj.cancel_multipart_upload(obj_name, mpu.id)
+            LOG.error(_LE("Some parts failed to upload to S3. "
+                          "Aborted the object key=%(obj_name)s") %
+                      {'obj_name': obj_name})
+            msg = (_("Failed to add image object to S3. "
+                     "key=%(obj_name)s") % {'obj_name': obj_name})
+            raise glance_store.BackendException(msg)
 
     def delete(self, location, context=None):
         """
@@ -559,3 +779,14 @@ def get_calling_format(bucket_format=None,
         return boto.s3.connection.OrdinaryCallingFormat()
     else:
         return boto.s3.connection.SubdomainCallingFormat()
+
+
+def get_mpu_xml(pedict):
+    xml = '<CompleteMultipartUpload>\n'
+    for pnum, etag in pedict.iteritems():
+        xml += '  <Part>\n'
+        xml += '    <PartNumber>%d</PartNumber>\n' % pnum
+        xml += '    <ETag>%s</ETag>\n' % etag
+        xml += '  </Part>\n'
+    xml += '</CompleteMultipartUpload>'
+    return xml
