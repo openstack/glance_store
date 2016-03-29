ba8e51f804beeb84c4524c4c59c4119b5100441e
kairat_kushaev <kkushaev@mirantis.com>
2016-03-29 10:25:13 +0300

    Deprecate swift driver options properly
    Some swift driver options were deprecated some time ago but it
    turns out that users are not aware of that. In order to notify
    users about deprecation and not breaking their envs after
    option removal we need to use deprecation flags in configuration
    options. So it must be shown in log files.
    
    Change-Id: I083eb6f00324b3a4b9a3313497993beab776cc52
    Related-Bug: #1561947

--- glance_store/_drivers/swift/utils.py
+++ glance_store/_drivers/swift/utils.py
@@ -30,21 +30,31 @@ swift_opts = [
     cfg.StrOpt('swift_store_auth_version', default='2',
                help=i18n._('Version of the authentication service to use. '
                            'Valid versions are 2 and 3 for keystone and 1 '
-                           '(deprecated) for swauth and rackspace. '
-                           '(deprecated - use "auth_version" in '
-                           'swift_store_config_file)')),
+                           '(deprecated) for swauth and rackspace.'),
+               deprecated_for_removal=True,
+               deprecated_reason=i18n._('Use "auth_version" in '
+                                        'swift_store_config_file.')
+               ),
     cfg.StrOpt('swift_store_auth_address',
                help=i18n._('The address where the Swift authentication '
-                           'service is listening. (deprecated - use '
-                           '"auth_address" in swift_store_config_file)')),
+                           'service is listening.'),
+               deprecated_for_removal=True,
+               deprecated_reason=i18n._('Use "auth_address" in '
+                                        'swift_store_config_file')
+               ),
     cfg.StrOpt('swift_store_user', secret=True,
                help=i18n._('The user to authenticate against the Swift '
-                           'authentication service (deprecated - use "user" '
-                           'in swift_store_config_file)')),
+                           'authentication service.'),
+               deprecated_for_removal=True,
+               deprecated_reason=i18n._('Use "user" in '
+                                        'swift_store_config_file.')),
     cfg.StrOpt('swift_store_key', secret=True,
                help=i18n._('Auth key for the user authenticating against the '
-                           'Swift authentication service. (deprecated - use '
-                           '"key" in swift_store_config_file)')),
+                           'Swift authentication service.'),
+               deprecated_for_removal=True,
+               deprecated_reason=i18n._('Use "key" in '
+                                        'swift_store_config_file.')
+               ),
     cfg.StrOpt('swift_store_config_file', secret=True,
                help=i18n._('The config file that has the swift account(s)'
                            'configs.')),
